941401e37026fd7ee6afcfb3fb84329b4bbc457f
will <elswicwj@rose-hulman.edu>
2013-09-09 19:16:21 -0400

    Adding my name!

--- helloWorld.c
+++ helloWorld.c
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
   printf("Hey, this is Mike McDonald!!!\n");
   printf("Hey, this is David Cooper\n");
   printf("This is Robert Fendricks\n");
+  printf("This is Will Elswick\n");
 
   return 0;
 }
