a88f86f1762f5a789d6bb4bf10c2c5103b4dff7c
Yashi <yadavy@YADAVY-W520.rose-hulman.edu>
2014-09-08 15:18:00 -0400

    my Name

--- helloWorld.c
+++ helloWorld.c
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
 
   printf("\n");
   printf("Hi this is Mark A. Yoder\n");
+  printf("Hi this is Yashi\n");
   // Add your name here.  Be sure it stills compiles
 
 
