e7f3e66545679e7596fe36d5fd428d7a3cd84310
koontz <koooontz@yahoo.com>
2016-09-02 15:09:45 -0400

    Added my name

--- helloWorld.c
+++ helloWorld.c
@@ -22,6 +22,7 @@ int main(int argc, char **argv)
 
   printf("\n");
   printf("Hi this is Mark A. Yoder\n");
+  printf("Hi this is Alvin Koontz\n");
   // Add your name below here.  Be sure it stills compiles
   return 0;
 }
