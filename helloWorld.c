74ffd124b2cb80fe00d151bba8ee32ed4ed05598
Charles Beard <beardce@rose-hulman.edu>
2017-09-07 12:32:52 -0400

    Added my name to the helloWorld.c file

--- helloWorld.c
+++ helloWorld.c
@@ -30,6 +30,7 @@ int main(int argc, char **argv)
   printf("Hello I am Joey Pierce\n");
   printf("Hi this is Dustin Kline\n");
   printf("Hi this is Donglai Guo\n");
+  printf("Hello, I'm Chace Beard\n");
 
   return 0
 }
