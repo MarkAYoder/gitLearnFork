10f074b5c064b430d9b83ccb61e9650eb0a68806
Mark A. Yoder <Mark.A.Yoder@Rose-Hulman.edu>
2023-12-05 13:36:28 -0500

    Update helloWorld.py for Winter 2023 2024

--- helloWorld.py
+++ helloWorld.py
@@ -1,30 +1,6 @@
 #!/usr/bin/env python3
-import Adafruit_BBIO.GPIO as GPIO
-import time
-
-LED="P9_11"
-
-GPIO.setup(LED, GPIO.OUT)
 
 print("Hello, World!")
 
-print("\r")
-
 print("Hi, this is Mark A. Yoder")
-print("Hi, this is Harris Wu")
 # Add your name below here.  Be sure it still runs.
-print("David Purdy")
-print("Jason Su")
-print("Eddie Mannan")
-print("Abel Keeley")
-print("Gaven Williams")
-print("Sophia Harrison")
-print("Brendan Perez")
-print("Angel Rivera")
-print("Donald Hau")
-print("Luke Wendel")
-print("Derick Miller")
-print("Skyler Cleland")
-print("Josh Mitterling")
-print("Glody Mutebwa")
-print("Martino Kim")
